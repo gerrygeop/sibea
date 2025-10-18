9382addfc7ed1c584ec3b05765ce7ecfcd4c0bc9
gerrygeop <gerryja28@gmail.com>
2025-10-18 12:27:57 +0800

    feat(migration): add pendaftaran migration

---
+++ database/migrations/2025_10_17_222844_create_pendaftarans_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pendaftarans', function (Blueprint $table) {
+            $table->id();
+
+            // Kolom Relasi
+            $table->foreignId('periode_beasiswa_id')->constrained('periode_beasiswas')->cascadeOnDelete();
+            $table->foreignId('mahasiswa_id')->constrained('mahasiswas')->cascadeOnDelete();
+
+            // Status Pendaftaran
+            $table->enum('status', ['draft', 'mendaftar', 'verifikasi', 'diterima', 'ditolak'])->default('draft');
+            $table->boolean('is_finalisasi')->default(false); // True jika mahasiswa sudah klik 'Kirim'
+
+            // KOLOM PENTING: Penyimpanan Path Berkas Upload
+            // Menyimpan semua path file yang diunggah mahasiswa (misal: {"upload_ktm": "path/file.pdf"})
+            $table->json('berkas_uploaded_json')->nullable();
+
+            // Kolom lain (opsional)
+            $table->string('note')->nullable(); // Feedback dari admin saat verifikasi
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Unik: 1 Mahasiswa hanya bisa mendaftar 1 kali per periode
+            $table->unique(['periode_beasiswa_id', 'mahasiswa_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pendaftarans');
+    }
+};
