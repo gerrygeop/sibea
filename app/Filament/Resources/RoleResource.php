32ec6940280e75d798148779372d6e245fcb423e
gerrygeop <gerryja28@gmail.com>
2025-11-03 10:46:46 +0800

    hide roles

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -21,6 +21,11 @@ class RoleResource extends Resource
     protected static ?string $navigationGroup = 'Manage Users';
     protected static ?int $navigationSort = 2;
 
+    public static function shouldRegisterNavigation(): bool
+    {
+        return false;
+    }
+
     public static function form(Form $form): Form
     {
         return $form
